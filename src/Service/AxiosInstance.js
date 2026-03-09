5e85d72958801ef1e0af95599a18c7803db21be0
Anjali198-hue <anjalibagdi923@gmail.com>
2026-03-09 17:50:16 +0530

    configure vector icons and app icons

--- src/Service/AxiosInstance.js
+++ src/Service/AxiosInstance.js
@@ -2,7 +2,7 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 
 // export const baseApiurl= 'https://api.sharkpocketinfra.in'
-export const baseApiurl = 'http://192.168.1.97:3001'
+export const baseApiurl = 'http://192.168.1.58:3001'
 
 const apiInstance = axios.create({
   baseURL: `${baseApiurl}`,
